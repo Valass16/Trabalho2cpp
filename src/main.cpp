da79beae4ec37bb161e052c844b7cd56be9942ad
Valass16 <joanete_despesa_8k@icloud.com>
2025-10-09 12:29:17 +0100

    5
    Passou a ter apenas 4 opções

--- src/main.cpp
+++ src/main.cpp
@@ -48,16 +48,7 @@ void opcao3() {
 
 void opcao4() {
     limparConsola();
-    cout << "\n=== OPÇÃO 4 SELECIONADA ===\n";
-    cout << "INC 4.\n";
-    pausar();
-    limparConsola();
-}
-
-void opcao5() {
-    limparConsola();
-    cout << "\n=== OPÇÃO 5 SELECIONADA ===\n";
-    cout << "INC 5.\n";
+    cout << "\nSair do programa.\n";
     pausar();
     limparConsola();
 }
@@ -69,9 +60,7 @@ void mostrarMenu() {
     cout << "1. Opção 1 - Descrição da opção 1\n";
     cout << "2. Opção 2 - Descrição da opção 2\n";
     cout << "3. Opção 3 - Descrição da opção 3\n";
-    cout << "4. Opção 4 - Descrição da opção 4\n";
-    cout << "5. Opção 5 - Descrição da opção 5\n";
-    cout << "0. Sair do programa\n";
+    cout << "4. Sair do programa\n";
     cout << "========================================\n";
     cout << "Digite sua opção: ";
 }
@@ -111,12 +100,6 @@ int main() {
                 break;
             case 4:
                 opcao4();
-                break;
-            case 5:
-                opcao5();
-                break;
-            case 0:
-                limparConsola();
                 cout << "\nA sair do programa...\n";
                 continuar = false;
                 break;
